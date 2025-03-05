1b479a3e661d277556267fc594bf320ebfea3096
phjpdev <jeanpatrick97816@gmail.com>
2025-03-05 21:52:52 +0300

    complete tab component

--- frontend/src/components/tabs/Tabs.js
+++ frontend/src/components/tabs/Tabs.js
@@ -17,36 +17,43 @@ const Tabs = () => {
         >
           主頁
         </div>
+        <div className='vertical-bar'></div>
         <div
           className={`tab ${activeTab === 'tab2' ? 'active' : ''}`}
           onClick={() => handleTabClick('tab2')}
         >
           受注時間
         </div>
+        <div className='vertical-bar'></div>
         <div
           className={`tab ${activeTab === 'tab3' ? 'active' : ''}`}
           onClick={() => handleTabClick('tab3')}
         >
           賽果/派彩
         </div>
+        <div className='vertical-bar'></div>
         <div
           className={`tab ${activeTab === 'tab4' ? 'active' : ''}`}
           onClick={() => handleTabClick('tab4')}
         >
           派彩快
+          <img src="https://consvc.hkjc.com//consvc.hkjc.com/-/media/Sites/JCBW/MenuIcons/icon-betslip/earlySettlement_svg/img-2406/Early-Settlement_default.svg?sc_lang=zh-HK" style={{marginLeft: '5px'}}></img>
         </div>
+        <div className='vertical-bar'></div>
         <div
           className={`tab ${activeTab === 'tab5' ? 'active' : ''}`}
           onClick={() => handleTabClick('tab5')}
         >
           過關/計數機
         </div>
+        <div className='vertical-bar'></div>
         <div
           className={`tab ${activeTab === 'tab6' ? 'active' : ''}`}
           onClick={() => handleTabClick('tab6')}
         >
           足智資料庫
         </div>
+        <div className='vertical-bar'></div>
         <div
           className={`tab ${activeTab === 'tab7' ? 'active' : ''}`}
           onClick={() => handleTabClick('tab7')}

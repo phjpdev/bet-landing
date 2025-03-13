8620473635133f5bf26b72ce98556f7e9bbc8bfb
phjpdev <jeanpatrick97816@gmail.com>
2025-03-13 05:21:38 +0300

    add date picker

--- frontend/src/components/record/Record.js
+++ frontend/src/components/record/Record.js
@@ -1,9 +1,16 @@
 import React, {useState, useEffect} from "react";
 import './Record.css';
+import { IoIosArrowDown } from "react-icons/io";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const Record = () => {
     const [activeTab, setActiveTab] = useState('record-header-tab3');
     const [currentDateTime, setCurrentDateTime] = useState("");
+    const [initialDateTime, setInitialDateTime] = useState("");
+    const [showRecordContainer, setShowRecordContainer] = useState(false);
+    const [startDate, setStartDate] = useState(new Date());
+    
     const handleTabClick = (tab) => {
         setActiveTab(tab);
     };
@@ -24,6 +31,7 @@ const Record = () => {
           }); // Format: HH:mm
     
           setCurrentDateTime(`${formattedDate} ${formattedTime}`);
+          setInitialDateTime(`${formattedDate}`);
         };
     
         updateDateTime(); // Set initial value
@@ -58,15 +66,53 @@ const Record = () => {
                     <div className="record-info-value balance">$925.20</div>
                 </div>
             </div>
-            <div className="record-info-container">
-                <div className="record-info-container-header">
-                    <div className="record-info-container-date">日期範圍 12/03/2025 - 12/03/2025</div>
-                    <div className="record-info-container-button">重新搜尋</div>
+            {showRecordContainer === false ? (
+                <div className="record-info-container">
+                    <div className="record-info-container-header">
+                        <div className="record-info-container-date">日期範圍 {initialDateTime} - {initialDateTime}</div>
+                        <div className="record-info-container-button" onClick={() => {setShowRecordContainer(true)}}>重新搜尋</div>
+                    </div>
+                    <div className="record-info-container-content">
+                        沒有交易記錄
+                    </div>
                 </div>
-                <div className="record-info-container-content">
-                    沒有交易記錄
+            ) : (
+                <div className="record-info-sec">
+                    <div className="record-info-search">
+                        <div className="date-picker-container">
+                            {/* Date Selection Box */}
+                            <div className="date-input-box" >
+                                <div><span>日期</span></div>
+                                <div style={{display:'flex',alignItems:'center', gap:'16px'}}>
+                                    <span className="date-display">{initialDateTime} - {initialDateTime}</span>
+                                    <span className="arrow-icon"><IoIosArrowDown style={{fontSize:'20px', display:'flex'}}/></span>
+                                </div>
+                            </div>
+                            <div style={{fontSize:'12px', marginTop:'8px', marginLeft:'24px'}}>每次最多可搜尋過去30天內其中8天。(以香港時間計算)</div>
+                        </div>
+                        <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+                        <div className="date-picker-container" style={{borderBottom:'none'}}>
+                            <div className="date-input-box">
+                                <div><span>交易種類</span></div>
+                                <div style={{display:'flex',alignItems:'center', gap:'16px'}}>
+                                    <span className="date-display">所有投注種類</span>
+                                    <span className="arrow-icon"><IoIosArrowDown style={{fontSize:'20px', display:'flex'}}/></span>
+                                </div>
+                            </div>
+                            <div className="date-input-box" style={{marginTop:'12px'}} >
+                                <div><span>顯示種類</span></div>
+                                <div style={{display:'flex',alignItems:'center', gap:'16px'}}>
+                                    <span className="date-display">所有</span>
+                                    <span className="arrow-icon"><IoIosArrowDown style={{fontSize:'20px', display:'flex'}}/></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="record-search-btn-div">
+                            <div className="record-search-btn">搜尋</div>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            )}
         </div>
     );
 }

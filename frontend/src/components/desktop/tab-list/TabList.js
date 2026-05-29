868f1e9331122a35f79d072b791d7737588c49fc
phjpdev <jeanpatrick97816@gmail.com>
2026-05-30 02:58:45 +0800

    sef

--- frontend/src/components/desktop/tab-list/TabList.js
+++ frontend/src/components/desktop/tab-list/TabList.js
@@ -10,7 +10,6 @@ const TabList = () => {
 
   return (
     <div className="tab-list-container">
-      {/* Tabs */}
       <div className="tabs">
         <div
           className={`tab ${activeTab === 'tab1' ? 'active' : ''}`}
@@ -26,27 +25,26 @@ const TabList = () => {
         </div>
       </div>
 
-      {/* Tab Content */}
       <div className="tab-content">
         {activeTab === 'tab1' && (
           <ul className="list">
+            <li>同場過關最高賠率勝出項目</li>
             <li>即時戰報</li>
             <li>名家分析</li>
             <li>
               <span className="new-item">
-                2分鐘學識過關派彩快 <span className="new-badge"><img src='/image/C_new.gif' alt='set-icon' width={12}></img></span>
+                2分鐘學識過關派彩快
               </span>
             </li>
             <li>單項派彩快介紹</li>
-            <li style={{borderBottom: 'none'}}>足球投注教學平台</li>
+            <li>足球投注教學平台</li>
           </ul>
         )}
 
         {activeTab === 'tab2' && (
           <ul className="list">
-            <li>「無效球賽」修訂</li>
             <li>馬會開戶口-全天候網上申請開戶</li>
-            <li style={{borderBottom: 'none'}}>有節制博彩</li>
+            <li>有節制博彩</li>
           </ul>
         )}
       </div>
